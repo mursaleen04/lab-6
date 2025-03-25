fad70b7e5e8c4ca6d20f32d7db2eb5443ec7e9ea
mursaleen <mursaleenarshad65@gmail.com>
2025-03-25 21:49:43 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,37 +1,24 @@
 ﻿using System;
-using q2;
 
-namespace Q2
+namespace lab_6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Shapes s1 = new Shapes(1, "Circle", "black");
-            Shapes s2 = new Shapes(2, "Triangle", "Blue");
-            Shapes s3 = new Shapes(3, "Rectangle", "yellow");
-            Shapes s4 = s1; 
-            Shapes s5 = new Shapes(s2); 
-          
+            Product product1 = new Product("MacBook", 1200, 4, 10);
+            Product product2 = new Product("iPad", 500, 5, 21);
+            Product product3 = new Product("iPhone 13", 150,6,32);
+            Product product4 = new Product();
 
-            Console.WriteLine("Shallow Copy:");
-            Console.WriteLine("----------------------------");
-            s4.Draw();
-            Console.WriteLine("----------------------------");
+            Store store = new Store("Ali Mobile Store", "Faisalabad");
 
-            Console.WriteLine("Deep Copy:");
-            Console.WriteLine("----------------------------");
-            s5.Draw();
-            Console.WriteLine("----------------------------");
+            store.AddProduct(product1);
+            store.AddProduct(product2);
+            store.AddProduct(product3);
+            store.AddProduct(product4);
 
-            Canvas c1 = new Canvas(12345);
-            c1.Addshape(s1);
-            c1.Addshape(s2);
-            c1.Addshape(s3);
-
-            Console.WriteLine("Displaying Canvas:");
-            Console.WriteLine("------------------------------------");
-            c1.Display();
+            store.Display();
 
             Console.ReadKey();
         }
