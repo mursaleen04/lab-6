42ae6de4f26dba2ea7c117fd9ba20c3739f555e4
mursaleen <mursaleenarshad65@gmail.com>
2025-03-25 21:44:52 +0500

    Add files via upload

---
+++ Canvas.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using q2;
+
+namespace Q2
+{
+    public class Canvas
+    {
+        public int CanvasID { get; set; }
+        private Shapes[] ShapesArray;
+        private int ShapeCount;
+
+        public Canvas(int id)
+        {
+            CanvasID = id;
+            ShapesArray = new Shapes[10];
+            ShapeCount = 0;
+        }
+
+        public Canvas(Canvas other)
+        {
+            CanvasID = other.CanvasID;
+            ShapeCount = other.ShapeCount;
+            ShapesArray = new Shapes[10];
+
+            for (int i = 0; i < ShapeCount; i++)
+            {
+                if (other.ShapesArray[i] != null)
+                    ShapesArray[i] = new Shapes(other.ShapesArray[i]);
+            }
+        }
+    
+
+        public void Addshape(Shapes shape) 
+        {
+            if (ShapeCount < ShapesArray.Length)
+            {
+                ShapesArray[ShapeCount] = shape;
+                ShapeCount++;
+                Console.WriteLine($"Shape {shape.Type} added to Canvas.");
+            }
+            else
+            {
+                Console.WriteLine("Canvas is full, cannot add more shapes.");
+            }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Canvas ID: {CanvasID}");
+            Console.WriteLine("All the shapes in the canvas:");
+            Console.WriteLine("--------------------------------");
+
+            if (ShapeCount == 0)
+            {
+                Console.WriteLine("No shapes added to the canvas.");
+                return;
+            }
+
+            for (int i = 0; i < ShapeCount; i++)
+            {
+                ShapesArray[i]?.Draw();
+                Console.WriteLine("----------------------------");
+            }
+        }
+    }
+}
---
+++ Shapes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q2
+{
+    internal class Shapes
+    
+        {
+            public int ShapeID { get; set; }
+            public string Type { get; set; }
+            public string Color { get; set; }
+
+            public Shapes(int shapeID, string shapeType, string color)
+            {
+                ShapeID = shapeID;
+                Type = shapeType;
+                Color = color;
+            }
+
+            public Shapes(Shapes other)
+            {
+                ShapeID = other.ShapeID;
+                Type = other.Type;
+                Color = other.Color;
+            }
+
+            public void Draw()
+            {
+                Console.WriteLine($"Shape ID: {ShapeID}");
+                Console.WriteLine($"Shape Type: {Type}");
+                Console.WriteLine($"Color: {Color}");
+            }
+
+            ~Shapes()
+            {
+                Console.WriteLine($"The shape {Type} with ID {ShapeID} is destructed.");
+            }
+        }
+    }
+
+
